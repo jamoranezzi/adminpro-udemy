5b98669e380a1bdf3d43ab11bbebe85e231ed220
andresmorale <andres.morales@emitekllc.com>
2018-07-21 14:02:32 -0400

    Sección 13 terminada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ServiceModule } from './services/service.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -28,7 +28,8 @@ import { FormsModule } from '@angular/forms';
     APP_ROUTES,
     PagesModule,
     ServiceModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+
 
 declare function init_plugins();
+//Google
+declare const gapi:any;
 
 @Component({
   selector: 'app-login',
@@ -10,15 +16,68 @@ declare function init_plugins();
 })
 export class LoginComponent implements OnInit {
 
-  constructor(public _router: Router) { }
+  email: string;
+  recuerdame:boolean = false;
+
+  //Google
+  auth2:any;
+
+  constructor(
+    public _router: Router,
+    public _usuarioService: UsuarioService
+  
+  ) { }
 
   ngOnInit() {
     init_plugins();
+    this.googleInit();
+
+    this.email = localStorage.getItem('email') || '';
+    if( this.email.length > 1) {
+      this.recuerdame = true;
+    }
+
   }
 
-  ingresar() {
+  googleInit(){
+    gapi.load('auth2', () => {
+        this.auth2 = gapi.auth2.init({
+            client_id: '1041195098351-tta4ktdk881mb6ofuat0jse4olt9ai86.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+            scope: 'profile email'
+        });
+        this.attachSignin( document.getElementById('btnGoogle'));
+    });
+  }
 
-    this._router.navigate([ '/dashboard']);
+  attachSignin( element ) {
+      
+    this.auth2.attachClickHandler( element, {}, (googleUser) => {
+
+          //let profile = googleUser.getBasicProfile();
+          let token = googleUser.getAuthResponse().id_token;
+
+          this._usuarioService.loginGoogle( token )
+          .subscribe( () => window.location.href = '#/dashboard' ); // No se pudo usar el router Navigate ya que hay un bug visual al hacer login con google.
+          
+    } );
+
+  }
+
+  ingresar( forma: NgForm) {
+
+    if( forma.invalid ){
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login(usuario, forma.value.recuerdame )
+      .subscribe(correcto => this._router.navigate(['/dashboard']));
+
+    // console.log(forma.valid);
+    // console.log(forma.value);
+    //this._router.navigate([ '/dashboard']);
 
   }
 
